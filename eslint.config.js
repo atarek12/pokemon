3ac57576b19ac1e71f57e06ca62e09a8b19e4251
Ahmed Tarek <a7md.tare2@gmail.com>
2025-07-21 19:29:43 +0300

    remove default template, setup eslint

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -14,10 +15,17 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+			eslintPluginPrettierRecommended
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      "prettier/prettier": ["warn", { endOfLine: "auto" }],
+      "@typescript-eslint/no-empty-object-type": "off",
+      "no-empty-pattern": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 ])
