a95204cf2a139ec99644140fd5853efae9e6575d
Ahmed Tarek <a7md.tare2@gmail.com>
2025-07-21 19:58:26 +0300

    setup chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import {
+  HStack,
+  Button,
+  ChakraProvider,
+  defaultSystem,
+} from "@chakra-ui/react";
+
 function App() {
-  return <div></div>;
+  return (
+    <ChakraProvider value={defaultSystem}>
+      <HStack>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+      </HStack>
+    </ChakraProvider>
+  );
 }
 
 export default App;
